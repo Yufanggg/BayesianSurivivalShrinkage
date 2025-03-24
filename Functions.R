d164c34258d6c5de4fb03f27cf040889b674e1c4
Yufanggg <wangalisa2017@gmail.com>
2025-03-24 16:04:28 +0100

    eval for prediction to be addressed

--- Functions.R
+++ Functions.R
@@ -74,76 +74,99 @@ Bayesian_Survival_model <- function(stan_data, baseline_assumption = "exponentia
 # @param: model, a model fitting by the bayesian method.
 # @param: the metric for model fitting criteria
 # @return: not return any things but give a few plots
-Bayesian_Survival_model_check <- function(bayesian_model_fit, criteria = c("Bayesian R-square", "MCMC chain", "MCMC autocorrelations")) {
+Bayesian_Survival_model_check <- function(bayesian_model_fit, criteria = c("MCMC chain", "MCMC autocorrelations")) {
   # Extract posterior samples
   posterior_samples <- extract(bayesian_model_fit)
-  # Convert posterior samples to mcmc object
-  mcmc_samples <- mcmc(as.matrix(posterior_samples))
   
-  # if ("Bayesian R-square" %in% criteria) { # might not be a useful metric for time-to-event data
-  #   Bayes_r2 <- bayes_R2(model)  # Calculate Bayesian R-square
-  #   mcmc_hist(Bayes_r2)  # Plot Bayesian R-square
-  # }
-
-  if ("MCMC chain" %in% criteria) {
+  for (i in 1:length(posterior_samples)){
+    # Convert posterior samples to mcmc object
+    mcmc_samples <- mcmc(as.matrix(posterior_samples[[i]]))
     # Plot MCMC chains
     plot(mcmc_samples)
-  }
-  
-  if ("MCMC autocorrelations" %in% criteria) {
     # Plot MCMC autocorrelations
     autocorr.plot(mcmc_samples)
   }
 }
 
-# Function to extract the results from the bayesian model that are useful for model comparsion.
-# @param: model, a model fitting by the bayesian method.
-# @param: the results to be extract from the fitted model
+# Function to extract the results from the Bayesian model that are useful for model comparison.
+# @param bayesian_model_fit: a model fitted by the Bayesian method.
+# @param criteria: the results to be extracted from the fitted model.
 # @return: a list including (1) the estimated coefficients for the design matrix (i.e., Beta_bayesian_est);
 #         (2) predictive survival probability for the testing dataset (i.e., sp); and (3) the information 
-#         of selected/non-selected variables(i.e., variableSelection): TRUE if selected, otherwise FALSE. 
-Bayesian_Survival_result_Extract<- function(model, criteria = c("DesignCoefficients", "Prediction_SurvivalProb", "variableSelection")) {
+#         of selected/non-selected variables (i.e., variableSelection): TRUE if selected, otherwise FALSE.
+
+Bayesian_Survival_result_Extract <- function(bayesian_model_fit, criteria = c("DesignCoefficients", "Prediction_SurvivalProb", "variableSelection")) {
   # Ensure the Output object is available
-  Output <- summary(model)$summary
+  Output <- summary(bayesian_model_fit)$summary
+  
+  # Initialize the result list
+  model_result <- list()
   
   if ("DesignCoefficients" %in% criteria) {
     Beta_bayesian_est <- Output[grep("^Beta", rownames(Output)), "mean", drop = FALSE]
-    print(Beta_bayesian_est)
+    model_result$Beta_bayesian_est <- Beta_bayesian_est
   }
   
   if ("Prediction_SurvivalProb" %in% criteria) {
     sp <- Output[grep("^survival_prob", rownames(Output)), "mean", drop = FALSE]
-    print(sp)
+    model_result$sp <- sp
   }
   
   if ("variableSelection" %in% criteria) {
     Beta_bayesian_est_LB <- Output[grep("^Beta", rownames(Output)), "2.5%", drop = FALSE]
     Beta_bayesian_est_UB <- Output[grep("^Beta", rownames(Output)), "97.5%", drop = FALSE]
-    variableSelection <- (Beta_bayesian_est_LB > 0) & (Beta_bayesian_est_UB < 0)
-    print(variableSelection)
+    includes_zero <- (Beta_bayesian_est_LB <= 0) & (Beta_bayesian_est_UB >= 0)
+    model_result$variableSelection <- !includes_zero 
   }
   
-  model_result <- list(
-    Beta_bayesian_est = Beta_bayesian_est,
-    sp = sp,
-    variableSelection = variableSelection
-  )
   return(model_result)
 }
 
 
-# Function to evaluate the  bayesian survival model with certain criteria
-# @model_result: a list of model results extracted from the bayesian survival model via Bayesian_Survival_result_Extract function.
-# @ComparsionValues: a list of ground-truth/model results extract from other models but organized in the same way with `model_result`.
-# @return: a list of model performance metric including 
-Model_comparsion <- function(model_result, ComparsionValues, ComparsionCriteria = c("MSE_est", "IBS", "CI", "Selection")){
+# Function to evaluate the Bayesian survival model with certain criteria
+# @model_result: a list of model results extracted from the Bayesian survival model via Bayesian_Survival_result_Extract function.
+# @ComparsionValues: a list of ground-truth/model results extracted from other models, including the following components: Beta_reference, test_t, test_status, and Selection_reference.
+# @return: a list of model performance metrics including MSE_est, IBS, C_index, and FDR.
+Model_performance_eval <- function(model_result, ComparsionValues, Criteria = c("MSE_est", "IBS", "C_index", "Selection")) {
   
+  model_metric <- list()
   
+  if ("MSE_est" %in% Criteria) {
+    Beta_reference <- ComparsionValues$Beta_reference
+    # EVAL 1: Evaluating the model performance at the level of Beta's.
+    Beta_bayesian_est <- model_result$Beta_bayesian_est # Extract the Betas
+    MSE_est <- (Beta_bayesian_est - Beta_reference) ** 2 # MSE_est is a vector with the length being the number of Betas
+    model_metric$rMSE <- MSE_est
+  }
   
-  model_metric = list(
-    MSE_est = MSE_est,
-    IBS = IBS,
-    CI = CI,
-    Selection = Selection,
-  )
+  if ("IBS" %in% Criteria) {
+    # EVAL 2: Evaluating the model performance at the level of prediction
+    test_t <- ComparsionValues$test_t
+    test_status <- ComparsionValues$test_status
+    
+    sp <- model_result$sp
+    Brier_score <- (sp - test_status)**2 # IBrier-score is a scalar
+    model_metric$Brier_score <- mean(Brier_score)
+  }
+  
+  if ("C_index" %in% Criteria) {
+    # EVAL 2: Evaluating the model performance at the level of prediction
+    sp <- model_result$sp
+    C_index <- Cindex(Surv(test_t, test_status), sp, test_t)
+    model_metric$C_index <- C_index
+  }
+  
+  if ("Selection" %in% Criteria) {
+    # EVAL 3: Evaluating the model variable selection
+    variableSelection_reference <- ComparsionValues$Selection_reference
+    selection <- model_result$variableSelection
+    FDR <- sum(selection & !variableSelection_reference) / sum(selection) # Only selected in the estimation but not for the ground-truth / selected in the estimation
+    model_metric$FDR <- FDR
+  }
+  
+  if (length(model_metric) == 0) {
+    stop("Sorry, we cannot evaluate the model without the criteria.")
+  }
+  
+  return(model_metric)
 }
